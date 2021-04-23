cb95481b2697d59e9d7a18bfd07edb052275b73a
hirosaki ryo <rih60909@gmail.com>
2021-04-23 15:44:58 +0900

    新規登録画面作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,15 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
   end
 
-  def show
+  def create
+    @user = User.new(params_user)
+    if @user.save
+      # ログインもする機能
+      redirect_to user_path, notice: "ユーザが作成されました！"
+    else
+      render :new
+    end
   end
 end
