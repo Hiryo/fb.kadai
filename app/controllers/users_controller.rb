3e546594635a19f9bc91d086f4167da66cb37b06
hirosaki ryo <rih60909@gmail.com>
2021-04-23 16:13:23 +0900

    ユーザーページ追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,10 +6,18 @@ class UsersController < ApplicationController
   def create
     @user = User.new(params_user)
     if @user.save
-      # ログインもする機能
-      redirect_to user_path, notice: "ユーザが作成されました！"
+      redirect_to user_path(@user.id), notice: "ユーザが作成されました！"
     else
       render :new
     end
+  end
+
+  def show
+    @users = User.find(params[:id])
+  end
+
+  private
+  def params_user
+  params.require(:user).permit(:name, :email, :password)
   end
 end
