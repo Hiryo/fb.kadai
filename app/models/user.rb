665d11bec022f3feab9b32028f6814f8215f5fa5
hirosaki ryo <rih60909@gmail.com>
2021-04-23 15:50:01 +0900

    validates追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,8 @@
 class User < ApplicationRecord
   has_many :blogs
+  has_secure_password
+
+  validates :name, presence: true, length: { maximum:30 }
+  validates :email, presence: true, length: { maximum:255 }, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i}, uniqueness: true
+  validates :password, length: {minimum: 6 }
 end
